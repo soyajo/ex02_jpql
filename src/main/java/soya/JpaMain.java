ecc1137a022fb097ed92411fb749151d337e224b
josoya <thdi4564@nate.com>
2023-03-07 17:23:29 +0900

    프로젝션

--- src/main/java/soya/JpaMain.java
+++ src/main/java/soya/JpaMain.java
@@ -37,24 +37,73 @@ public class JpaMain {
              * 결과가 없거나 둘 이상이면 에러 반환
              */
 
+//            Member member = new Member();
+//            member.setUsername("member1");
+//            member.setAge(10);
+//            em.persist(member);
+//
+//            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+//            List<Member> resultList = query.getResultList();
+//
+//            for (Member member1 : resultList) {
+//                System.out.println("member1 = " + member1);
+//            }
+//
+//            // 값이 없으면 에러, 여러개가 있어도 에러
+////            Member singleResult = em.createQuery("select m from Member m where m.id = 10L", Member.class).getSingleResult();
+////            System.out.println("singleResult = " + singleResult);
+//
+//            Member singleResultMember1 = em.createQuery("select m from Member m where m.username = :username", Member.class).setParameter("username", "member1").getSingleResult();
+//            System.out.println("singleResultMember1 = " + singleResultMember1.getUsername());
+
+            /**
+             * 프로젝션
+             * - select 절에 조회할 대상을 지정하는 것
+             * - 프로젝션 대상 : 엔티티, 임베디드 타입, 스칼라 타입(숫자, 문자 등 기본 데이터 타입)
+             * - select m from Member m -> 엔티티 프로젝션
+             * - select m.team from Member m -> 엔티티 프로젝션
+             * - select m.address from Member m -> 임베디드 프로젝션
+             * - select m.username, m.age from Member m -> 스칼라 타입 프로젝션
+             * - distinct 로 중복 제거
+             *
+             */
             Member member = new Member();
             member.setUsername("member1");
             member.setAge(10);
             em.persist(member);
 
-            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-            List<Member> resultList = query.getResultList();
+            em.flush();
+            em.clear();
 
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
-            }
+            // 조인 쿼리가 나감.
+            List<Team> teams = em.createQuery("select t from Member m join m.team t", Team.class).getResultList();
+            System.out.println("teams = " + teams);
 
-            // 값이 없으면 에러, 여러개가 있어도 에러
-//            Member singleResult = em.createQuery("select m from Member m where m.id = 10L", Member.class).getSingleResult();
-//            System.out.println("singleResult = " + singleResult);
+            List<Address> addresses = em.createQuery("select o.address from Order o", Address.class).getResultList();
+            System.out.println("addresses = " + addresses);
 
-            Member singleResultMember1 = em.createQuery("select m from Member m where m.username = :username", Member.class).setParameter("username", "member1").getSingleResult();
-            System.out.println("singleResultMember1 = " + singleResultMember1.getUsername());
+            /**
+             * 프로젝션 - 여러 값 조회 방법 3가지
+             * select m.username ,m.age from Member m
+             * 1. Query 타입으로 조회
+             * 2. Object[] 타입으로 조회
+             * 3. new 명령어로 조회
+             *  - 단순 값을 DTO로 바로 조회
+             *  - select new soya.domain.UserDTO(m.username, m.age) from Member m
+             *  - 패키지 명을 포함한 전체 클래스 명 입력
+             *  - 순서와 타입이 일치하는 생성자 필요
+             *
+             */
+
+            List<MemberDTO> resultList = em.createQuery("select new soya.domain.MemberDTO(m.username, m.age) from Member m", MemberDTO.class).getResultList();
+            MemberDTO memberDTO = resultList.get(0);
+
+            System.out.println("memberDTO.getName() = " + memberDTO.getName());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
+
+            /**
+             *
+             */
 
 
             ts.commit();
